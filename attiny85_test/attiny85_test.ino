2c6f2f2084f243b8b6733093adc0a9f66834717f
Travis M <snp@s>
2019-12-22 16:56:46 -0500

    Add pulsing light as test for ATTiny85 board

---
+++ attiny85_test/attiny85_test.ino
@@ -0,0 +1,37 @@
+// the setup routine runs once when you press reset:
+void setup() {                
+  // initialize the digital pin as an output.
+  pinMode(0, OUTPUT); //LED on Model B
+  pinMode(1, OUTPUT); //LED on Model A   
+}
+
+
+// the loop routine runs over and over again forever:
+void loop() {
+  pulse(3, 100);
+  delay(2000);
+}
+
+void pulse(int quantity, long blinkDurationInMillisecons) {
+  for (int i = 0; i < quantity; i++) {
+    blink(blinkDurationInMillisecons);
+  }
+}
+
+void blink(long durationInMilliseconds) {
+  lighton();
+  delay(durationInMilliseconds);
+  lightoff();
+  delay(durationInMilliseconds);
+}
+
+void lighton() {
+  digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(1, HIGH);
+}
+
+void lightoff() {
+  digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(1, LOW); 
+}
+
